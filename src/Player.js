45636abc46b34ec9bcd5765b49a77b37a7b58410
noe <noe.reyes92@yahoo.com>
2021-10-02 11:30:33 -0500

    first commit

---
+++ src/Player.js
@@ -0,0 +1,19 @@
+
+
+// class Player extends Component {
+//     constructor() {
+//         this.state = {
+//             name: 'default Name'
+//         }
+//         this.funnyName = this.funnyName.bind(this);
+//     }
+
+//     funnyName() {
+//         this.setState({ name: 'Booger' });
+//         return "Booger"
+//     }
+
+//     render() {
+//         return <h1>You smell {this.funnyName()}</h1>
+//     }
+// }
