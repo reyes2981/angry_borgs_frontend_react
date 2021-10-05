4484d6a2a2fa9751f8ac9272b534cccc95a4af3a
noe <noe.reyes92@yahoo.com>
2021-10-05 10:38:43 -0500

    iterated trhough tasks array and displayed to browser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import Header from "./components/Header";
+import { useState } from 'react'
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
 
 function App() {
-  
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Doctors Appointent',
+            day: 'Oct 8th at 10:30am',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Meeting at School',
+            day: 'Oct 10th at 01:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'Food Shopping',
+            day: 'Oct 6th at 07:30am',
+            reminder: false
+        }
+    ]
+)
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,35 +1,15 @@
-import Task from './Task'
-
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointent',
-        day: 'Oct 8th at 10:30am',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Oct 10th at 01:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Oct 6th at 07:30am',
-        reminder: false
-    }
-]
-
-const Tasks = ( {tasks} ) => {
+const Tasks = ( {tasks}) => {
+    
 
     return (
         <>
            {tasks.map((task) => (
-               <Task key={task.id} task={task} />
+               <h3 key={task.id}>{task.text}</h3>
            ))} 
         </>
     )
 }
 
-export default Tasks
+export default Tasks 
+
+//state is one way data
